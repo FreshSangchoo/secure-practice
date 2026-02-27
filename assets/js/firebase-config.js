c8f5c4a3c0c2f99e65fcab3c372455fcfb884000
FreshSangchoo <aprldpdy289@naver.com>
2026-02-27 11:06:11 +0900

    [update] firestore rule

---
+++ assets/js/firebase-config.js
@@ -0,0 +1,13 @@
+/* ========================================
+   Firebase 설정값 - git에 올리지 않음 (.gitignore)
+   새 키 발급 후 이 파일에만 입력하세요
+   ======================================== */
+export const firebaseConfig = {
+  apiKey:            'AIzaSyCqchTnLHC0I4xApiM4ifyXr78T33avUNo',
+  authDomain:        'aws--memo.firebaseapp.com',
+  projectId:         'aws--memo',
+  storageBucket:     'aws--memo.firebasestorage.app',
+  messagingSenderId: '664815808028',
+  appId:             '1:664815808028:web:d054cb61decf579fc079d9',
+  measurementId:     'G-0RETMESLK7',
+};
